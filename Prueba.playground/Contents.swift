8f435048d00f79b5151ea4908c80acee5defe10a
carlos ayala <carlosayala@macbookpro.local>
2016-10-14 17:21:28 -0600

    Modificaciones arreglos

--- Prueba.playground/Contents.swift
+++ Prueba.playground/Contents.swift
@@ -11,3 +11,21 @@ var ancho = 10
 print((altura) * (ancho))
 
 
+var a: Int = 4
+var b: Int = 5
+
+a += 1 //a++
+//arreglos
+var idiomas = ["español","chino","ingles","frances"]
+
+idiomas.count
+idiomas+=["portugues","madarin"]
+idiomas.count
+idiomas[0]
+
+idiomas[2]
+
+idiomas[5]
+
+
+
