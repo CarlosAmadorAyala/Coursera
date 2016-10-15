73303855e274efe1adb1a2bc273dd8831e1268c1
carlos ayala <carlosayala@macbookpro.local>
2016-10-15 11:59:14 -0600

    Operador if con and y or

--- Semana2.playground/Contents.swift
+++ Semana2.playground/Contents.swift
@@ -77,8 +77,7 @@ for mes in meses{
 //variables
 //al menos un caso
 //una seccion de default
-var meses = 1...12
-
+/*var meses = 1...12
 for mes in meses{
     switch mes {
     case 1,2,3:
@@ -91,3 +90,50 @@ for mes in meses{
         print("clima desconocido")
     }
 }
+
+
+for i in 10...20
+{
+    switch i {
+    case 12...15:
+        print("inferior: \(i)")
+    case 16...20:
+        print("superior: \(i)")
+    default:
+        print("error")
+    }
+}
+*/
+
+40 < 50 //menor
+40 > 50 //mayor
+40 <= 50 //menor o igula
+40 >= 50 //mayor o igual
+40 != 50 //diferente
+40 == 50 //igual
+
+let peso = 70.0
+let altura = 1.60
+let imc = peso / (altura * altura)
+
+
+if imc > 40.0 {
+    print("Obesidad")
+    
+}else if imc > 30.0 && imc < 40.0{
+    print("Grado de obesidad")
+}else if imc > 25.0 && imc < 30.0{
+    print("Sobrepeso")
+}
+
+if true && true {
+    print("Es el operador AND")
+}
+
+
+if false || true {
+    print("Es el operador OR")
+}
+
+
+
