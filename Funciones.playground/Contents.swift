7ded1506fbe985f865842711b938b69a042b54e7
carlos ayala <carlosayala@macbookpro.local>
2016-10-18 11:09:38 -0600

    Valor de Retorno

--- Funciones.playground/Contents.swift
+++ Funciones.playground/Contents.swift
@@ -4,16 +4,24 @@ import UIKit
 
 var str = "Hello, playground"
 
-func calcularIdicieDeMasaCorporal(peso : Double, altura : Double)
-{
+//Valor de retorno
+func calcularIdicieDeMasaCorporal(peso : Double, altura : Double) -> Double{
     //var peso = 70.0
     //var altura = 1.60
-    let imc = peso / (altura * altura)
-    print(imc)
+    //let imc = peso / (altura * altura)
+    //return imc
+    
+    return peso / (altura * altura)
+    //print(imc)
 }
 
-calcularIdicieDeMasaCorporal(peso: 78.00, altura: 1.72)
+let imc = calcularIdicieDeMasaCorporal(peso: 78.00, altura: 1.72)
 
 calcularIdicieDeMasaCorporal(peso: 74.00, altura: 1.72)
 
 calcularIdicieDeMasaCorporal(peso: 90.00, altura: 1.80)
+
+
+
+print(imc)
+print(calcularIdicieDeMasaCorporal(peso: 90.00, altura: 1.80))
