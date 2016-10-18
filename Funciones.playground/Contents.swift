5ccb201f3f0f6da548b2d44ccfdbc3f949590227
carlos ayala <carlosayala@macbookpro.local>
2016-10-18 11:24:19 -0600

    Tuplas

--- Funciones.playground/Contents.swift
+++ Funciones.playground/Contents.swift
@@ -1,27 +1,53 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
+/*
 var str = "Hello, playground"
 
 //Valor de retorno
-func calcularIdicieDeMasaCorporal(peso : Double, altura : Double) -> Double{
+func calcularIdicieDeMasaCorporal(pesoIntegral peso : Double, altura : Double) -> Double{
     //var peso = 70.0
     //var altura = 1.60
     //let imc = peso / (altura * altura)
     //return imc
-    
+    //return 0.0
     return peso / (altura * altura)
     //print(imc)
 }
 
-let imc = calcularIdicieDeMasaCorporal(peso: 78.00, altura: 1.72)
+let imc = calcularIdicieDeMasaCorporal(pesoIntegral: 78.00, altura: 1.72)
 
-calcularIdicieDeMasaCorporal(peso: 74.00, altura: 1.72)
+calcularIdicieDeMasaCorporal(pesoIntegral: 74.00, altura: 1.72)
 
-calcularIdicieDeMasaCorporal(peso: 90.00, altura: 1.80)
-
-
+calcularIdicieDeMasaCorporal(pesoIntegral: 90.00, altura: 1.80)
 
 print(imc)
-print(calcularIdicieDeMasaCorporal(peso: 90.00, altura: 1.80))
+print(calcularIdicieDeMasaCorporal(pesoIntegral: 90.00, altura: 1.81))
+*/
+//Coleccion dicionario o arreglo
+//tuplas (combinaciones de tipos de datos de regreso)
+
+
+
+func calcularIdicieDeMasaCorporal(pesoIntegral peso : Double, altura : Double) ->(Double,String){
+    //var peso = 70.0
+    //var altura = 1.60
+    let imc = peso / (altura * altura)
+    var mensaje = ""
+        if (imc > 18.50 && imc < 25.00){
+            mensaje = "Peso normal"
+        }else {
+            mensaje = "Debes consultar a tu medico"
+        }
+    let resultado = (imc,mensaje)
+    return resultado
+    //return imc
+    //return 0.0
+    //return peso / (altura * altura)
+    //print(imc)
+}
+
+let resultadoImc = calcularIdicieDeMasaCorporal(pesoIntegral: 74, altura: 1.70)
+
+resultadoImc.0
+resultadoImc.1
