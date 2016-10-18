1b4e7c86b346d966b31e0b1ea3d774ce3f392eb8
carlos ayala <carlosayala@macbookpro.local>
2016-10-18 10:48:20 -0600

    Actualizacion

--- Funciones.playground/Contents.swift
+++ Funciones.playground/Contents.swift
@@ -4,14 +4,16 @@ import UIKit
 
 var str = "Hello, playground"
 
-func calcularIMC()
+func calcularIdicieDeMasaCorporal(peso : Double, altura : Double)
 {
-var peso = 70.0
-var altura = 1.60
-let imc = peso / (altura * altura)
+    //var peso = 70.0
+    //var altura = 1.60
+    let imc = peso / (altura * altura)
     print(imc)
 }
 
-calcularIMC()
-calcularIMC()
-calcularIMC()
+calcularIdicieDeMasaCorporal(peso: 78.00, altura: 1.72)
+
+calcularIdicieDeMasaCorporal(peso: 74.00, altura: 1.72)
+
+calcularIdicieDeMasaCorporal(peso: 90.00, altura: 1.80)
