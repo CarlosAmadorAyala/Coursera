3b29a8046453d44a14305f785d69a5263daaaa66
carlos ayala <carlosayala@macbookpro.local>
2016-10-18 11:39:27 -0600

    Tuplas v2

--- Funciones.playground/Contents.swift
+++ Funciones.playground/Contents.swift
@@ -27,7 +27,7 @@ print(calcularIdicieDeMasaCorporal(pesoIntegral: 90.00, altura: 1.81))
 //Coleccion dicionario o arreglo
 //tuplas (combinaciones de tipos de datos de regreso)
 
-
+/**
 
 func calcularIdicieDeMasaCorporal(pesoIntegral peso : Double, altura : Double) ->(Double,String){
     //var peso = 70.0
@@ -51,3 +51,46 @@ let resultadoImc = calcularIdicieDeMasaCorporal(pesoIntegral: 74, altura: 1.70)
 
 resultadoImc.0
 resultadoImc.1
+//Ejemplo
+func a(nombre : String)-> (Bool,String)
+{
+return (true,"b")
+}
+
+a(nombre:"a")
+ **/
+func calcularIdicieDeMasaCorporal(pesoIntegral peso : Double, altura : Double) ->(imcCalculado: Double, mensajeDeSalida: String){
+    //var peso = 70.0
+    //var altura = 1.60
+    let imc = peso / (altura * altura)
+    var mensaje = ""
+    if (imc > 18.50 && imc < 25.00){
+        mensaje = "Peso normal"
+    }else {
+        mensaje = "Debes consultar a tu medico"
+    }
+    let resultado = (imc,mensaje)
+    return resultado
+    //return imc
+    //return 0.0
+    //return peso / (altura * altura)
+    //print(imc)
+}
+
+//let (imc,_) = calcularIdicieDeMasaCorporal(pesoIntegral: 74, altura: 1.70)
+
+//let (_,mensaje) = calcularIdicieDeMasaCorporal(pesoIntegral: 74, altura: 1.70)
+
+let imcTupla = calcularIdicieDeMasaCorporal(pesoIntegral: 74, altura: 1.70)
+
+imcTupla.imcCalculado
+imcTupla.mensajeDeSalida
+
+var pelicula : (nombre: String, añoDeSalida: Int, calificacion : Double) = ("Gool", 2006, 5.5)
+
+
+pelicula.añoDeSalida
+pelicula.nombre
+pelicula.calificacion
+
+
